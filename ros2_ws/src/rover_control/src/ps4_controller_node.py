0be1185ec7d3342092e7a97064d5914aef539922
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-06-22 18:04:54 -0400

    Updates ps4 controller node

--- ros2_ws/src/rover_control/src/ps4_controller_node.py
+++ ros2_ws/src/rover_control/src/ps4_controller_node.py
@@ -1,10 +1,10 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from std_msgs.msg import Float64MultiArray
 from pyPS4Controller.controller import Controller
 
 
-def hundo(current_num):
+def map_to_torque(current_num):
     current_min = -32767
     current_max = 32767
 
@@ -17,16 +17,18 @@ def hundo(current_num):
 
     return desired_number
 
+# ros2 topic pub /drive_controller/commands std_msgs/msg/Float64MultiArray "data: [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]""
+
 
 class MyROS2Node(Node):
     def __init__(self):
         super().__init__('my_ros2_node')
         self.publisher = self.create_publisher(
-            String, '/dif_cont/cmd_vel_unstamped', 10)
+            Float64MultiArray, '/drive_controller/commands', 10)
 
     def publish_data(self, data):
-        msg = String()
-        msg.data = data
+        msg = Float64MultiArray()
+        msg.data = [1.0, 1.0, 1.0]
         self.publisher.publish(msg)
         self.get_logger().info(f"Published data: {msg.data}")
 
@@ -37,11 +39,11 @@ class MyController(Controller):
         self.ros2_node = ros2_node
 
     def on_L3_up(self, value):
-        a = hundo(value)
-        self.ros2_node.publish_data('L3 up:{}'.format(a))
+        a = map_to_torque(value)
+        self.ros2_node.publish_data(a)
 
     def on_L3_down(self, value):
-        a = hundo(value)
+        a = map_to_torque(value)
         self.ros2_node.publish_data('L3 down:{}'.format(a))
 
     def on_L3_x_at_rest(self):
@@ -51,11 +53,11 @@ class MyController(Controller):
         self.ros2_node.publish_data('L3 rest')
 
     def on_R3_up(self, value):
-        a = hundo(value)
+        a = map_to_torque(value)
         self.ros2_node.publish_data('R3 up:{}'.format(a))
 
     def on_R3_down(self, value):
-        a = hundo(value)
+        a = map_to_torque(value)
         self.ros2_node.publish_data('R3 down:{}'.format(a))
 
     def on_R3_x_at_rest(self):
